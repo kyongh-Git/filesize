be262525d420f997d874f52f96f33be0c1c64532
kyongh-Git <55642439+kyongh-Git@users.noreply.github.com>
2021-03-18 17:27:46 -0500

    Add files via upload

--- filesize_check.js
+++ filesize_check.js
@@ -1,5 +1,4 @@
 $(function() {
-$("#submit_form").click(function(e){
 $('#uploadifive_upload_element_1').bind('change', function() {
         alert('This file size is: ' + this.files[0].size/1024/1024 + "MiB");
     });
