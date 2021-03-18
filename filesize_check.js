8f02d9e5b5849a9a28c7f2319c1c9ca4e48ca407
kyongh-Git <55642439+kyongh-Git@users.noreply.github.com>
2021-03-18 17:40:30 -0500

    Add files via upload

--- filesize_check.js
+++ filesize_check.js
@@ -1,6 +1,13 @@
-$(function() {
+/*$(function() {
 $('#uploadifive_upload_element_1').bind('change', function() {
         alert('This file size is: ' + this.files[0].size/1024/1024 + "MiB");
     });
 });
+});*/
+
+$(document).ready(function ()
+{
+	  $('#uploadifive_upload_element_1').bind('change', function() {
+        alert('This file size is: ' + this.files[0].size/1024/1024 + "MiB");
+    });
 });
